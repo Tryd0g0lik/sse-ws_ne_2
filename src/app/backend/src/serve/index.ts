19d20315be8af3a9e38617b0d6eaa624cfdabaae
Tryd0g0lik <work80@mail.ru>
2023-10-18 15:55:07 +0700

    test

--- src/app/backend/src/serve/index.ts
+++ src/app/backend/src/serve/index.ts
@@ -9,6 +9,4 @@ const db = require('db/index.js');
 const app = new Koa();
 const server = http.createServer(app.callback);
 const wss = new WS.Server({ server });
-let newClient = {};
-let postmane: any;
-let url: string = '';
+
